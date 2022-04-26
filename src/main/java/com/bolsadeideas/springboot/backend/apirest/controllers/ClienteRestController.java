bdddfae6ce6a1b1aa1ca15730b6be7af649c1f80
Martín <magadiflo@gmail.com>
2022-04-26 12:31:24 -0500

    Escribiendo los métodos show y create en el Controlador Backend API Rest

--- src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
+++ src/main/java/com/bolsadeideas/springboot/backend/apirest/controllers/ClienteRestController.java
@@ -3,9 +3,14 @@ package com.bolsadeideas.springboot.backend.apirest.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
@@ -22,6 +27,18 @@ public class ClienteRestController {
 	@GetMapping("/clientes")
 	public List<Cliente> index() {
 		return this.clienteService.findAll();
+	}
+
+	@GetMapping("/clientes/{id}")
+	public Cliente show(@PathVariable Long id) {
+		return this.clienteService.findById(id);
+	}
+
+	// @RequestBody, el objeto cliente es enviado dentro del cuerpo del request
+	@PostMapping("/clientes")
+	@ResponseStatus(HttpStatus.CREATED)
+	public Cliente create(@RequestBody Cliente cliente) {
+		return this.clienteService.save(cliente);
 	}
 
 }
